8c382014db182dd0e3b4e30d6a8bf241d9e72b7f
Alex Semёnov <84786361+NotDevelopper@users.noreply.github.com>
2021-06-05 17:38:04 +0700

    Quick search on VK
    Use MxPacker to make .mxaddon package

---
+++ MX-VK-Search/music.js
@@ -0,0 +1,5 @@
+javascript:
+selected=window.getSelection();
+var myRuntime = window.external.mxGetRuntime();
+var mxTabs = myRuntime.create('mx.browser.tabs');
+var TabNum = mxTabs.newTab({url:'https://vk.com/search?c[q]='+encodeURIComponent(selected)+'&c[section]=audio',activate: true,position:"afterCurrrent"});
