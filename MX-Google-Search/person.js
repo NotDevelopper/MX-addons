37448bc8f86f1f66a4848f16b23745a287296fc7
Alex Semёnov <84786361+NotDevelopper@users.noreply.github.com>
2021-06-15 16:10:13 +0700

    Unofficial MX add-on for Google Search
    Use MxPacker to make .mxaddon package

---
+++ MX-Google-Search/person.js
@@ -0,0 +1,5 @@
+javascript:
+selected=window.getSelection();
+var myRuntime = window.external.mxGetRuntime();
+var mxTabs = myRuntime.create('mx.browser.tabs');
+var TabNum = mxTabs.newTab({url:'https://www.google.com/search?q='+encodeURIComponent(selected)+'&tbm=pers', activate: true,position:"afterCurrrent"});
